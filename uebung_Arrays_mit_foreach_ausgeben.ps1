20e85d77c341d8189d1c96f63e4e94d77269f111
your name <your email address>
2019-09-05 22:53:37 +0200

    Create uebung_Arrays_mit_foreach_ausgeben.ps1
    eine kleine Übung zum Umgang mit Arrays in PS-Objekten

---
+++ uebung_Arrays_mit_foreach_ausgeben.ps1
@@ -0,0 +1,23 @@
+﻿clear
+
+
+$Apps = Get-AppPackage *Desk*
+foreach ($App in $Apps)
+{
+        if ($App.Dependencies.Count -gt 0) # Filtert alle Apps die keine Abhängigkeiten haben.
+        {
+            Write-host $App.Name '(Abhängigkeiten:' $App.Dependencies.Count ')'
+            echo ***********************
+
+            $Deps = $App.Dependencies
+            foreach ($Dep in  $Deps) 
+            {
+                Write-Host $Dep
+            }
+
+        }
+
+    Start-Sleep -Seconds 1
+    echo ""
+    
+}
